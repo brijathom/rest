57ee2df530a2e2966af4d512f8b1dde7047d688b
Brian Thomas <brianjamesthomas@live.com>
2021-05-09 20:12:17 +0100

    Correct a mistake in the comments

--- src/main/java/com/example/rest/repository/CountryRepository.java
+++ src/main/java/com/example/rest/repository/CountryRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 // A repository is an interface that lets
 // you perform various operations involving Country objects.
 // Most of these operations are obtained by extending the
-// MongoRepository interface that is defined in the Spring frameword.
+// MongoRepository interface that is defined in the Spring framework.
 @Repository
 public interface CountryRepository extends MongoRepository<Country, String> {
     public Country findByCode(String code);
